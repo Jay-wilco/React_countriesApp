45e98b1cf217c7a7179b2780050e265a61fb6b38
Jay-wilco <jtvilkko@gmail.com>
2026-01-13 13:25:30 +0200

    add fix to solve deployment issue in AuthContext  import

--- src/app/countries/[slug]/page.js
+++ src/app/countries/[slug]/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { useAuth } from "../../app/context/AuthContext";
+import { useAuth } from "../../context/AuthContext";
 import FavouriteButton from "../../components/FavouriteButton";
 import {
   clearSelectedCountry,
