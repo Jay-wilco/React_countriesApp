39f40a0cf7e0ef545a0f5eb78f28a27fae2fb032
Jay-wilco <jtvilkko@gmail.com>
2026-01-13 15:13:26 +0200

    Add webpack alias @ to src

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  turbopack: {
-    resolveAlias: {
-      "@": "./src",
-    },
-  },
-
   images: {
     remotePatterns: [
       {
@@ -19,6 +19,11 @@ const nextConfig = {
         pathname: "/**",
       },
     ],
+  },
+
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.join(__dirname, "src");
+    return config;
   },
 };
 
